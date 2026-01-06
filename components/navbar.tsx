3c4e44357112fbb78762d3157ecd4381647618c0
Matias Freire <matiaspfreire@gmail.com>
2026-01-05 20:05:11 -0500

    navbar update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,17 +7,17 @@ export default function Navbar() {
   return (
     //nav container
     <nav
-      className="flex justify-between items-center bg-[rgba(26,26,26,0.4)] px-9.5 py-5.75 rounded-[10px] shadow-[0_4px_30px_rgba(0,0,0,0.1)] border border-[rgba(255,255,255,0.1)] backdrop-blur-md fixed w-full z-100 max-w-370 mx-auto mt-7 mb-5 left-1/2 top-0 transform -translate-x-1/2"
+      className="flex justify-between items-center bg-[rgba(26,26,40,0.7)] px-12 py-6 rounded-xl shadow-xl border border-[rgba(255,255,255,0.15)] backdrop-blur-xl fixed w-full z-100 max-w-300 mx-auto mt-7 mb-5 left-1/2 top-0 transform -translate-x-1/2 transition-all duration-500"
     >
       {/* Logo (left side) */}
       <div>
-        <Link href="/" className="text-white text-[1.5rem] font-bold no-underline tracking-[1px] pl-5">
+        <Link href="/" className="text-white text-[1.5rem] font-bold no-underline tracking-[1px] pl-2">
           Matias Freire
         </Link>
       </div>
 
       {/* Navigation Links (center/right) */}
-      <ul className="flex gap-12.5 list-none m-0 pr-20 ">
+      <ul className="flex gap-12.5 list-none m-0 pr-8 ">
         <li> 
           <Link href="/" className="text-[#ccc] text-[1rem] no-underline uppercase transition-colors duration-300 relative overflow-hidden px-1.25 hover:text-white">
             Home
@@ -47,7 +47,7 @@ export default function Navbar() {
       </ul>
 
       {/* LinkedIn and GitHub Icons & Links */}
-      <ul className="flex gap-6 list-none m-0 p-0 pr-20 ">
+      <ul className="flex gap-6 list-none m-0 p-0 pr-10 ">
 
         <li>
           <a href="https://github.com/MatiasPF1"target="_blank"rel="noopener noreferrer"className="text-[#ccc] hover:text-white text-2xl transition-colors duration-300"
