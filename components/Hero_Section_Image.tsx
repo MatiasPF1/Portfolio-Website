0e10c5da33bd6aeae0ba2e2c3ad5120d7660f8da
Matias Freire <matiaspfreire@gmail.com>
2026-01-07 14:33:45 -0500

    update of image and status of hero

--- components/Hero_Section_Image.tsx
+++ components/Hero_Section_Image.tsx
@@ -1,18 +1,46 @@
 import Image from "next/image";
 
+import { CiLocationOn } from "react-icons/ci"; //Location Icon
+import { LuGraduationCap } from "react-icons/lu"; //Graduation Cap Icon
+import { MdWorkOutline } from "react-icons/md"; // Work Outline Icon
+
+
+
+
 export default function Hero_Section() {
   return (
-    // Right Section - Hero Image
-    <div className="relative h-105 w-88 ml-290 mt-50  rounded-3xl shadow-sm">
+    <div className="relative h-105 w-88 ml-290 mt-50 rounded-3xl shadow-sm">
       <Image
-        src="/Me.jpeg"
+        src="/Matias.jpeg"
         alt="Hero Image"
         fill
         className="object-cover rounded-xl"
         priority
       />
-    </div>   
-  
- 
+
+  {/* Status of Hero */}
+<div className="absolute left-6 bottom-6 translate-y-46 z-10 grid grid-cols-[auto_1fr] gap-x-4 gap-y-6  py-4  ">
+
+
+  {/* Location */}
+  <CiLocationOn size={24} className="text-white" />
+  <span className="text-white text-sm leading-none self-center">
+    New York Metropolitean Area
+  </span>
+
+  {/* Education */}
+  <LuGraduationCap size={24} className="text-white" />
+  <span className="text-white text-sm leading-none self-center">
+    Bachelor's in computer science, 2028
+  </span>
+
+  {/* Work */}
+  <MdWorkOutline size={24} className="text-white" />
+  <span className="text-white text-sm leading-none self-center">
+    DL Research Assistant at Advanced Quantumn Materials Lab
+  </span>
+</div>
+     
+    </div>
   );
 }
