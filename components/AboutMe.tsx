c3d2726a057a71ed66fa67c3d08f5884633ff007
Matias Freire <matiaspfreire@gmail.com>
2026-01-09 20:21:48 -0500

    Music section - Done

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import Shuffle from '@/Reactbits_Components/Shuffletext';
 
 export default function About_Me() {
@@ -40,7 +39,7 @@ export default function About_Me() {
       </div>
 
       {/* A Piece Of My Life*/}
-      <div className="flex justify-center mb-8 mr-[84rem]">
+      <div className="flex justify-center mb-8 mr-336 ">
         <Shuffle
           text="A Piece Of My Life"
           shuffleDirection="left"
@@ -52,9 +51,11 @@ export default function About_Me() {
           loopDelay={2}
         />
       </div>
+      <p className="text-xl mt-4 text-center leading-relaxed pt-[1rem]"><strong>Some photos I love i took during my First/Second Year of College</strong> </p>
+
 
       {/* Photo Grid */}
-      <div className="grid grid-cols-4 gap-18 px-15 max-w-440 mx-auto mb-7">
+      <div className="grid grid-cols-4 gap-18 px-15 max-w-440 mx-auto mb-7 pt-[2rem]">
         {lifeMoments.map((moment, index) => (
           <div key={index} className="relative group pt-10">
             <div className="overflow-hidden rounded-lg  transition-transform hover:scale-105 mb-5">
@@ -70,7 +71,7 @@ export default function About_Me() {
       </div>
 
       {/* Music */}
-      <div className="flex justify-center mt-24 mr-408">
+      <div className="flex justify-center mr-408 pt-[4rem] ">
         <Shuffle
           text="Music"
           shuffleDirection="left"
@@ -81,6 +82,99 @@ export default function About_Me() {
           loop
           loopDelay={2}
         />
+      </div>
+      <p className="text-xl mt-4 text-center leading-relaxed pt-[1rem]"><strong>Some of the Music I enjoy</strong> </p>
+
+      
+      {/* Music Grid */}
+      <div className="grid grid-cols-4 gap-7 px-8 max-w-7xl mx-auto mb-[24rem] pt-[3rem]">
+        <div className="relative group">
+          <div className="overflow-hidden rounded-lg transition-transform hover:scale-105 ">
+            <img
+              src="/Malcom.jpeg"
+              alt="Malcom Todd"
+              className="w-full aspect-square object-cover"
+            />
+          </div>
+          <p className="text-xs mt-4 text-center leading-relaxed">Malcom Todd</p>
+        </div>
+        
+        <div className="relative group">
+          <div className="overflow-hidden rounded-lg transition-transform hover:scale-105 ">
+            <img
+              src="/bruno-mars.jpg"
+              alt="Bruno Mars"
+              className="w-full aspect-square object-cover"
+            />
+          </div>
+          <p className="text-xs mt-4 text-center leading-relaxed">Bruno Mars</p>
+        </div>
+        
+        <div className="relative group">
+          <div className="overflow-hidden rounded-lg transition-transform hover:scale-105 ">
+            <img
+              src="/Enanitos_verdes.jpg"
+              alt="Enanitos Verdes"
+              className="w-full aspect-square object-cover"
+            />
+          </div>
+          <p className="text-xs mt-4 text-center leading-relaxed">Enanitos Verdes</p>
+        </div>
+        
+        <div className="relative group">
+          <div className="overflow-hidden rounded-lg transition-transform hover:scale-105 ">
+            <img
+              src="/Grent.jpeg"
+              alt="Grent Perez"
+              className="w-full aspect-square object-cover"
+            />
+          </div>
+          <p className="text-xs mt-4 text-center leading-relaxed">Grent Perez</p>
+        </div>
+        
+        <div className="relative group">
+          <div className="overflow-hidden rounded-lg  transition-transform hover:scale-105 ">
+            <img
+              src="/Jane_Remover.jpg"
+              alt="Jane Remover"
+              className="w-full aspect-square object-cover"
+            />
+          </div>
+          <p className="text-xs mt-4 text-center leading-relaxed">Jane Remover</p>
+        </div>
+        
+        <div className="relative group">
+          <div className="overflow-hidden rounded-lg transition-transform hover:scale-105 ">
+            <img
+              src="/Laufey1.jpg"
+              alt="Laufey"
+              className="w-full aspect-square object-cover"
+            />
+          </div>
+          <p className="text-xs mt-4 text-center leading-relaxed">Laufey</p>
+        </div>
+        
+        <div className="relative group">
+          <div className="overflow-hidden rounded-lg transition-transform hover:scale-105 ">
+            <img
+              src="/TheMarias.jpg"
+              alt="The Marias"
+              className="w-full aspect-square object-cover"
+            />
+          </div>
+          <p className="text-xs mt-4 text-center leading-relaxed">The Marias</p>
+        </div>
+        
+        <div className="relative group">
+          <div className="overflow-hidden rounded-lg transition-transform hover:scale-105 ">
+            <img
+              src="/Tame_Impala.jpg"
+              alt="Tame Impala"
+              className="w-full aspect-square object-cover"
+            />
+          </div>
+          <p className="text-xs mt-4 text-center leading-relaxed">Tame Impala</p>
+        </div>
       </div>
 
     </section>
