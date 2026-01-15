df0bb163bf0ad0967dfe69b97a93dd47bf87e2f3
Matias Freire <matiaspfreire@gmail.com>
2026-01-15 16:35:09 -0500

    first try to fix the video display on safari/edge of the null display of the video background

--- components/BackgroundVideo.tsx
+++ components/BackgroundVideo.tsx
@@ -1,15 +1,30 @@
 'use client';
 
+import { useEffect, useRef } from 'react';
+
 export default function BackgroundVideo() {
+  //Used to force execution of the video 
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play().catch(() => {});
+    }
+  }, []);
+
   return (
     <div className="fixed inset-0 z-0">
       <video
+        ref={videoRef}
         className="w-full h-full object-cover"
         src="/Particles.mp4"
         autoPlay
         muted
         loop
         playsInline
+        preload="auto"
+        controls={false}
+        disablePictureInPicture
+        webkit-playsinline="true"
       />
     </div>
   );
