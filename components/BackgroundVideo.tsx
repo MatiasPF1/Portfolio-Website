e1c7959b235559f4f7c3b2cbf64f27538d91ab19
Matias Freire <matiaspfreire@gmail.com>
2026-01-19 11:13:57 -0500

    static background for mobile

--- components/BackgroundVideo.tsx
+++ components/BackgroundVideo.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useRef } from 'react';
+import Image from 'next/image';
 
 export default function BackgroundVideo() {
   //Used to force execution of the video 
@@ -16,16 +17,31 @@ export default function BackgroundVideo() {
 
   return (
     <div className="fixed inset-0 z-0">
+      
+      {/* Mobile Background Image - less than (768px) */}
+      <div className="block md:hidden w-full h-full">
+        <Image
+          src="/MobileBackGround.png"
+          alt="Mobile Background"
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
+      
+      {/* Desktop Background Video - visible on screens md (768px) and larger */}
       <video
         ref={videoRef}
-        className="w-full h-full object-cover"
+        className="hidden md:block w-full h-full object-cover"
         autoPlay
         muted
         loop
         playsInline
       >
-      <source src="/Particles.mp4" type="video/mp4" />
+        <source src="/Particles.mp4" type="video/mp4" />
       </video>
+
+
     </div>
   );
 }
